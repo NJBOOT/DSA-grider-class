b9fc7ecb9c6a6a43bbff2836d047a87dbfdce617
Nico Barbarese <nicobarbarese@gmail.com>
2020-12-09 00:07:21 -0500

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    let subArr = array.slice(index, index + size);
+    chunked.push(subArr);
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let res = [];
+//   while (array.length) {
+//     let temp = array.slice(0, size);
+//     array.splice(0, size);
+//     res.push(temp);
+//   }
+//   return res;
+// }
+
+// function chunk(array, size) {
+//     let chunked = [];
+//     for (el of array) {
+//       const last = chunked[chunked.length - 1];
+//       if (!last || last.length === size) {
+//         chunked.push([el]);
+//       } else {
+//         last.push(el);
+//       }
+//     }
+//     return chunked;
+//   }
