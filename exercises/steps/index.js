203bf7ebc008e3a5c838550c77c8ba63727eb1fb
Nico Barbarese <nicobarbarese@gmail.com>
2020-12-10 00:08:26 -0500

    recurse stairs

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    let stair = "#".repeat(i) + " ".repeat(n - i);
+    console.log(stair);
+  }
+}
+
+function recurseSteps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (stair.length === n) {
+    console.log(stair);
+    return recurseSteps(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  recurseSteps(n, row, stair);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//     let steps = new Array(n).fill(" ");
+//     for (let i = 0; i < n; i++) {
+//       steps[i] = "#";
+//       console.log(steps.join(""));
+//     }
+//   }
+
+// function steps(n) {
+//     let steps = new Array(n).fill(" ");
+//     for (let step in steps){
+//       steps[step] = "#"
+//       console.log(steps.join(""))
+//     }
+//   }
+
+// function steps(n) {
+//     for (let i = 0; i < n; i++) {
+//       let stairs = "";
+//       for (j = 0; j < n; j++) {
+//         if (j <= i) {
+//           stairs += "#";
+//         } else {
+//           stairs += " ";
+//         }
+//       }
+//       console.log(stairs);
+//     }
+//   }
