fb5ce12537185a7bb2dee8eb3b345818e4a87c8d
Nico Barbarese <nicobarbarese@gmail.com>
2020-12-13 10:52:22 -0500

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Recursive
+function pyramid(n, row = 0, level = "") {
+  let levelLength = n * 2 - 1;
+  let midLength = Math.floor(levelLength / 2);
+  if (n === row) {
+    return;
+  }
+  if (level.length === levelLength) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  let add;
+  if (midLength - row <= level.length && midLength + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  return pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// iteritive
+// function pyramid(n) {
+//     const levelLength = n * 2 - 1;
+//     const midLength = Math.floor(levelLength / 2);
+//     for (let row = 0; row < n; row++) {
+//       let level = "";
+//       for (let col = 0; col < levelLength; col++) {
+//         if (col >= midLength - row && col <= midLength + row) {
+//           // console.log(`col: ${col}, midLength:${midLength}`);
+//           level += "#";
+//         } else {
+//           level += " ";
+//         }
+//       }
+//       console.log(level);
+//     }
+//   }
